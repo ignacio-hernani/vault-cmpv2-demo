235dde4bd6fc755fa9291736584d15df9b9da7f2
ignacio-hernani <89387442+ignacio-hernani@users.noreply.github.com>
2024-11-07 16:01:22 +0100

    Update 02-new-rsa-2048-key-csr.sh

--- 02-new-rsa-2048-key-csr.sh
+++ 02-new-rsa-2048-key-csr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -nodes -newkey rsa:2048 -keyout "${CLIENT_KEY}" -out "${CLIENT_CSR}"
+openssl req -nodes -newkey rsa:2048 -keyout "${CLIENT_KEY}" -out "${CLIENT_CSR}" -subj "/CN=super.example.com"
